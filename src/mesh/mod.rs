4b62862cdfd64aac0210b1c39b73de5d2c96bfbc
wattrg <robertwhat@hotmail.com>
2021-02-15 18:02:32 +1000

    remove incomplete 1 and 3D meshes

--- src/mesh/mod.rs
+++ src/mesh/mod.rs
@@ -1,11 +1,11 @@
-/// one dimensional mesh
-pub mod cartesian1d;
+// /// one dimensional mesh
+// pub mod cartesian1d;
 
 /// two dimensional mesh
 pub mod cartesian2d;
 
-/// three dimensional mesh
-pub mod cartesian3d;
+// /// three dimensional mesh
+// pub mod cartesian3d;
 
 /// 1, 2, or 3 dimensional mesh
 pub mod cartesian_any_d;
